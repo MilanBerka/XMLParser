31c369d42a2601101a33d438e475b4a2fa814981
MilanBerka <berkamilanml@gmail.com>
2017-03-23 17:06:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 18 17:37:48 2017
-
-@author: Milan
-"""
+#from __future__ import print_function
 import pip
-pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'pydrive'])
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'httplib2'])
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'google-api-python-client'])
 
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from io import BytesIO
-import os 
+import httplib2
+import os
+import io 
 import zipfile 
 import pandas as pd
-import numpy as np
 import xml.etree.ElementTree as ET
-import glob
 from keboola import docker
 
-""" =========================== """
-"""     PARSER DEFINITON        """
-""" =========================== """
-## UNIQUE ID GENERATOR (NOT USED)
-#def uniqueIdGenerator():
-#    seed = 1
-#    while True:
-#       yield seed
-#       seed += 1
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaIoBaseDownload
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+CFG = docker.Config()
+PARAMS = CFG.get_parameters()
+FOLDER_NAMES = PARAMS.get('folderNames')
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = PARAMS.get('scope')
+CLIENT_SECRET_FILE = PARAMS.get('cljson')
+APPLICATION_NAME = 'Drive API Python Quickstart'
 
 # MAIN PARSER CLASSES
 class Node:
@@ -117,7 +121,7 @@ class Node:
 
 class XMLParser:
     """
-    XML Parser for Liftago. Takes either `path_to_xml` string (mandatory, set to None if unused) or `extTree` ElementTree
+    XML Parser for Liftago. Takes either `path_to_xml` string (mandatory, set to None if unusued) or `extTree` ElementTree
     object. Method `parseToDataFrame` returns flattened xml as pandas.DataFrame, which can be then exported to csv via *.to_csv method
     """
     def __init__(self,path_to_xml,extTree=None):
@@ -139,63 +143,99 @@ class XMLParser:
         if returnDataFrame:
             return self.xmlDataFrame
         
-""" =========================== """
-"""    GOOGLE API CONNECTION    """
-""" =========================== """
+def get_credentials():
+    """Gets valid user credentials from storage.
 
-if __name__ == '__main__': 
-    cfg = docker.Config()
-    parameters = cfg.get_parameters()
-    folderNames = parameters.get('folderNames')
-    ### Keboola adjustments must be made        
-    # os.chdir('C:/Users/Milan/Documents/Liftago/KBC/Python/data/in/files/CSOB_batches')
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-    drive = GoogleDrive(gauth)
-    ###
-    
-    """ =========================== """
-    """    GOOGLE DRIVE DEFS    """
-    """ =========================== """
-    # alreadyExtractedXML2CSV = [i.split('\\')[1].split('_')[0] for i in glob.glob('csv/*.csv')]
-    # Name of folders with zipfiles (aka '../FOLDER_TO_LOOKAT/zipfiles'). 
-    # EXACT NAMES REQUIRED !!!
-    # NO DUPLICATE NAMES !!!
-    if folderNames:
-        FOLDERS_TO_LOOKAT = list(folderNames)
-    else:
-        FOLDERS_TO_LOOKAT = ['CSOB AM 2016','CSOB AM 2017'] 
-    
-    finalDataFrame = None
-    
-    for folderToLookAt in FOLDERS_TO_LOOKAT:
-        driveFilesList = drive.ListFile({'q':"mimeType='application/vnd.google-apps.folder' and title='{}' and trashed=false".format(folderToLookAt)}).GetList()                        
-        folderId = driveFilesList[0]['id']
-        zipfilesInFolder = drive.ListFile({'q':"'{}' in parents".format(folderId)}).GetList()
-        for zf in zipfilesInFolder:
-            if 'zip' in zf['title'].lower():
-                print('title: {}'.format(zf['title']))
-                toUnzip = drive.CreateFile({'id':zf['id']})
-                toUnzipStringContent = toUnzip.GetContentString(encoding='cp862')
-                toUnzipBytesContent = BytesIO(toUnzipStringContent.encode('cp862'))
-                readZipfile = zipfile.ZipFile(toUnzipBytesContent, "r")
-                for fileInZipfileName in readZipfile.namelist():
-                    if '.xml' in fileInZipfileName.lower():
-                        openedXml = readZipfile.open(fileInZipfileName).read()
-                        loadedXml = ET.fromstring(openedXml.decode())
-                        toBeParsed = XMLParser(None,loadedXml.find('merchants'))  
-                        parsedXmlDataFrame = toBeParsed.parseToDataFrame()
-                        if finalDataFrame:
-                            finalDataFrame = pd.concat([finalDataFrame.copy(),parsedXmlDataFrame])
-                        else:
-                            finalDataFrame = parsedXmlDataFrame.copy()
-                    
-                    else:
-                        pass              
-            else:
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def extract_xml2csv(readZipfile,finalDataFrame=None):
+    for fileInZipfileName in readZipfile.namelist():
+        if '.xml' in fileInZipfileName.lower():
+            if ('-T' in fileInZipfileName.upper()) or ('-M' in fileInZipfileName.upper()):
                 pass
-            
-    try:
-        finalDataFrame.to_csv('data/out/tables/parsedBatch.csv',index=None)
-    except AttributeError:
-        pass
+            else:
+                openedXml = readZipfile.open(fileInZipfileName).read()
+                loadedXml = ET.fromstring(openedXml.decode())
+                toBeParsed = XMLParser(None,loadedXml.find('merchants'))  
+                parsedXmlDataFrame = toBeParsed.parseToDataFrame()
+                if finalDataFrame:
+                    finalDataFrame = pd.concat([finalDataFrame.copy(),parsedXmlDataFrame])
+                else:
+                    finalDataFrame = parsedXmlDataFrame.copy()
+        else:
+            pass              
+def main():
+    """Shows basic usage of the Google Drive API.
+
+    Creates a Google Drive API service object and outputs the names and IDs
+    for up to 10 files.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    results = service.files().list(
+        pageSize=10,fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+if __name__ == '__main__':
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v3', http=http)
+
+    results = service.files().list(q="mimeType = 'application/vnd.google-apps.folder' \
+    and name = 'CSOB AM 2017' and trashed=false",
+        pageSize=50,fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        finalDataFrame = None
+        desiredFileId = items[0]['id']
+        driveZipFiles = service.files().list(q="'{}' in parents and trashed=false and (mimeType contains 'zip') ".format(desiredFileId),
+        pageSize=1000,fields="nextPageToken, files(id, name)").execute()
+        zipitems = driveZipFiles.get('files', [])
+        if not zipitems:
+            print('No files found.')
+        print('Files:')
+        for item in zipitems:
+            pass
+            request = service.files().get_media(fileId=item['id'])
+            fh = io.BytesIO()
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+            readZipfile = zipfile.ZipFile(fh, "r")
+            extract_xml2csv(readZipfile,finalDataFrame)
+        
+        finalDataFrame.to_csv('/data/out/tables/parsedBatch.csv',index=None) 
